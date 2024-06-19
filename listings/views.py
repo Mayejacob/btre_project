0a29c69953e8dbdb2985992aa9d0b1fafe7642d4
mayejacob <mayejacob3@gmail.com>
2024-06-19 23:09:23 +0100

    upated

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from .models import Listing
+from .choices import price_choices, bedroom_choices, state_choices
 
 
 def index(request):
@@ -15,7 +16,26 @@ def index(request):
     return render(request, 'listings/listings.html', context)
     
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {
+        'listing': listing
+    }
+    return render(request, 'listings/listing.html', context)
     
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list.filter(description__icontains=keywords)
+        
+        
+    context = {
+        'price_choices': price_choices, 
+        'bedroom_choices': bedroom_choices, 
+        'state_choices': state_choices,
+        'listings': queryset_list,
+    }
+    return render(request, 'listings/search.html', context)
